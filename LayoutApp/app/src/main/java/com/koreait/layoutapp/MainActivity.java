106f9d1d9fdbf4524370a9d316550d3b59369f6c
kaaang <kaaang@kakao.com>
2021-07-06 21:11:09 +0900

    0706

--- LayoutApp/app/src/main/java/com/koreait/layoutapp/MainActivity.java
+++ LayoutApp/app/src/main/java/com/koreait/layoutapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.koreait.layoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.q7); //화면에 보여질 뷰를 XML을 이용하는 방법
+
+/*
+        //xml이 아닌 순수 자바 코드로 디자인을 구현해보자
+        Button bt = new Button(this);
+        bt.setText("button");
+
+        //버튼의 width, height 지정
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        bt.setLayoutParams(layoutParams);
+        setContentView(bt);//버튼은 view를 상속받으니까
+
+ */
     }
 }
